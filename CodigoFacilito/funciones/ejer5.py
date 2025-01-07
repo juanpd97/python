9636a33642e540316d1912dedfa37825c586f796
Jp <juanpdominguez97@gmail.com>
2025-01-07 06:01:13 -0300

    funciones ejer5

--- CodigoFacilito/funciones/ejer5.py
+++ CodigoFacilito/funciones/ejer5.py
@@ -6,3 +6,24 @@ La función debe retornar True o False si todos los elementos de la lista son el
 - La función retornará None si la lista se encuentra vacía o solo posee un elemento.
 """
 
+def condicion(unaFuncion):
+    def nueva_funcion(unaLista):
+        if len(unaLista) <= 1:
+            return None
+        else: return unaFuncion(unaLista)
+    return nueva_funcion
+
+
+@condicion
+def iguales(unaLista):
+    aux = unaLista[0]
+    for elemento in unaLista:
+        if elemento != aux:
+            return False
+    return True
+
+# Pruebas de la función
+print(iguales([1, 1, 1])) # Debe retornar True 
+print(iguales([1, 2, 1])) # Debe retornar False 
+print(iguales([])) # Debe retornar None 
+print(iguales([1])) # Debe retornar None
